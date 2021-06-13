b871a4a246b4fd9d4f596f042542a43f18a20f61
Martin Hauner <martin.hauner@gmx.net>
2021-06-13 17:50:32 +0200

    add file type & json schema

---
+++ src/main/kotlin/io/openapiprocessor/intellij/TypeMapping.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2021 https://github.com/openapi-processor/openapi-processor-intellij
+ * PDX-License-Identifier: Apache-2.0
+ */
+
+package io.openapiprocessor.intellij
+
+import com.intellij.openapi.fileTypes.LanguageFileType
+import com.intellij.openapi.util.IconLoader
+import org.jetbrains.yaml.YAMLLanguage
+import javax.swing.Icon
+
+class TypeMapping : LanguageFileType(YAMLLanguage.INSTANCE, true) {
+
+    override fun getName(): String {
+        return "openapi-processor mapping"
+    }
+
+    override fun getDescription(): String {
+        return "OpenAPI-Processor Configuration"
+    }
+
+    override fun getDefaultExtension(): String {
+        return ""
+    }
+
+    override fun getIcon(): Icon {
+        return IconLoader.getIcon("/META-INF/pluginIcon.svg")
+    }
+
+}
---
+++ src/main/kotlin/io/openapiprocessor/intellij/TypeMappingSchema.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 https://github.com/openapi-processor/openapi-processor-intellij
+ * PDX-License-Identifier: Apache-2.0
+ */
+
+package io.openapiprocessor.intellij
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.impl.http.HttpsFileSystem
+import com.jetbrains.jsonSchema.extension.JsonSchemaFileProvider
+import com.jetbrains.jsonSchema.extension.JsonSchemaProviderFactory
+import com.jetbrains.jsonSchema.extension.SchemaType
+
+val SCHEMA_URL = "raw.githubusercontent.com/openapi-processor/openapi-processor-core/master/src/main/resources/mapping/v2/mapping.yaml.json"
+
+class TypeMappingSchema: JsonSchemaProviderFactory {
+
+    override fun getProviders(project: Project): MutableList<JsonSchemaFileProvider> {
+        return mutableListOf(TypeMappingSchemaProvider)
+    }
+
+    object TypeMappingSchemaProvider: JsonSchemaFileProvider {
+        private val schema = HttpsFileSystem.getHttpsInstance().findFileByPath(SCHEMA_URL)
+
+        override fun isAvailable(file: VirtualFile): Boolean {
+            return file.fileType is TypeMapping
+        }
+
+        override fun getName(): String {
+            return "openapi-processor mapping"
+        }
+
+        override fun getSchemaFile(): VirtualFile? {
+            return schema
+        }
+
+        override fun getSchemaType(): SchemaType {
+            return SchemaType.remoteSchema
+        }
+
+    }
+
+}
