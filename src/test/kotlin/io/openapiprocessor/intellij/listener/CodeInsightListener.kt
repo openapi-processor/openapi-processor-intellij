fd56f01959e1686d559d2ba433301763291a1a93
Martin Hauner <martin.hauner@gmx.net>
2021-07-15 13:08:17 +0200

    extract test setup

---
+++ src/test/kotlin/io/openapiprocessor/intellij/listener/CodeInsightListener.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2021 https://github.com/openapi-processor/openapi-processor-intellij
+ * PDX-License-Identifier: Apache-2.0
+ */
+
+package io.openapiprocessor.intellij.listener
+
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture
+import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory
+import com.intellij.testFramework.fixtures.impl.LightTempDirTestFixtureImpl
+import io.kotest.core.listeners.TestListener
+import io.kotest.core.test.TestCase
+import io.kotest.core.test.TestResult
+
+class CodeInsightListener: TestListener {
+    lateinit var fixture: CodeInsightTestFixture
+
+    override suspend fun beforeTest(testCase: TestCase) {
+        val factory = IdeaTestFixtureFactory.getFixtureFactory()
+        val builder = factory.createLightFixtureBuilder()
+        val temp = LightTempDirTestFixtureImpl(true)
+        fixture = factory.createCodeInsightFixture(builder.fixture, temp)
+        fixture.setUp()
+    }
+
+    override suspend fun afterTest(testCase: TestCase, result: TestResult) {
+        fixture.tearDown()
+    }
+
+}
