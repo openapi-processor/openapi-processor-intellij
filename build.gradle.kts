a223a29c6c8827641f4fa27f48eb8fd98bb7d784
Martin Hauner <martin.hauner@gmx.net>
2022-04-16 08:36:01 +0200

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ fun properties(key: String) = project.findProperty(key).toString()
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.5.10"
-    id("org.jetbrains.intellij") version "1.2.0"
+    id("org.jetbrains.intellij") version "1.2.1"
     id("org.jetbrains.changelog") version "1.1.2"
     id("com.github.ben-manes.versions") version "0.39.0"
 
