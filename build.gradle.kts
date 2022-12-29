0153bc0f6de4f1db43e94776923aab76f9679005
Martin Hauner <martin.hauner@gmx.net>
2022-12-29 14:12:28 +0100

    allow intellij plugin snapshots

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ fun properties(key: String) = project.findProperty(key).toString()
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.6.21"
-    id("org.jetbrains.intellij") version "1.10.1"
+    id("org.jetbrains.intellij") version "1.10.1"  // 1.11 is broken, 1.11.2-SNAPSHOT works again
     id("org.jetbrains.changelog") version "1.1.2"
     id("com.github.ben-manes.versions") version "0.42.0"
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
 rootProject.name = "openapi-processor-intellij"
+
+pluginManagement {
+    repositories {
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+        gradlePluginPortal()
+    }
+}
