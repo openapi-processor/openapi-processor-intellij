25bcafc65c7cc4bb0a7f339086ae979107212929
Martin Hauner <martin.hauner@gmx.net>
2023-07-02 15:03:19 +0200

    update dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.6.21"
+    id("org.jetbrains.kotlin.jvm") version "1.7.21"
     id("org.jetbrains.intellij") version "1.14.2"
     id("org.jetbrains.changelog") version "2.1.0"
     id("com.github.ben-manes.versions") version "0.47.0"
@@ -15,7 +15,6 @@ plugins {
 group = properties("pluginGroup")
 version = properties("pluginVersion")
 
-// Configure project's dependencies
 repositories {
     mavenCentral()
 }
