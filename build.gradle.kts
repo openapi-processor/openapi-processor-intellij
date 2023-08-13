3ad029e6a0979e8e8dffed215597cf239c1882da
Martin Hauner <martin.hauner@gmx.net>
2023-08-13 08:40:09 +0200

    clean up

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.8.22"
     id("org.jetbrains.intellij") version "1.15.0"
-    id("org.jetbrains.changelog") version "2.1.2" // "1.2.1"  // newer versions are broken
+    id("org.jetbrains.changelog") version "2.1.2"
     id("com.github.ben-manes.versions") version "0.47.0"
 }
 
