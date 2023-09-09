6a98ec466dd57f65a32b2af2511579be36d03d13
Martin Hauner <martin.hauner@gmx.net>
2023-09-09 10:09:45 +0200

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.8.22"
     id("org.jetbrains.intellij") version "1.15.0"
-    id("org.jetbrains.changelog") version "2.1.2"
+    id("org.jetbrains.changelog") version "2.2.0"
     id("com.github.ben-manes.versions") version "0.48.0"
 }
 
