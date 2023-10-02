dbed5cb0ad9d3f8973d93b0fe0788dd725d3ac74
Martin Hauner <martin.hauner@gmx.net>
2023-10-02 18:38:19 +0200

    not public anymore

--- src/main/kotlin/io/openapiprocessor/intellij/OpenApiLineMarker.kt
+++ src/main/kotlin/io/openapiprocessor/intellij/OpenApiLineMarker.kt
@@ -3,6 +3,10 @@ package io.openapiprocessor.intellij
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider
 import com.intellij.psi.PsiElement
+import com.intellij.psi.search.searches.ReferencesSearch
+//import com.intellij.swagger.index.OpenapiSpecificationContentIndex
+import org.jetbrains.yaml.psi.YAMLKeyValue
+import org.jetbrains.yaml.psi.YAMLMapping
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -14,6 +18,86 @@ class OpenApiLineMarker: RelatedItemLineMarkerProvider()  {
         result: MutableCollection<in RelatedItemLineMarkerInfo<*>>
     ) {
 
+        log.warn("language {} {}", element.language, element.text)
+//        val isMapping = isMappingFile(element.containingFile)
+//        if (!isMapping)
+
+//        if (element.text != TypeMappingLineMarker.PACKAGE_KEY)
+//            return
+
+        val isMethod = Methods.methods.contains(element.text) // LeafPsiElement
+        if (!isMethod) {
+            return
+        }
+
+
+        // is path?
+//        val path = element.text.startsWith("/")
+//        if (!path) {
+//            return
+//        }
+
+//        element.children.forEach {
+//            log.warn("element child {}", it.text)
+//        }
+
+        val elementKeyValue = element.parent
+        if (elementKeyValue !is YAMLKeyValue)
+            return
+
+        elementKeyValue.children.forEach {
+            log.warn("parent child\n {}", it.text)
+        }
+
+        val yamlMapping = elementKeyValue.parent
+        if (yamlMapping !is YAMLMapping)
+            return
+
+        val xxx = yamlMapping.parent
+        log.warn("xxx {}", xxx.text)
+
+
+        yamlMapping.children.forEach {
+            log.warn("parent 2 child {}", it.text)
+        }
+
+        val file = element.containingFile
+
+//        val apiFiles = OpenapiSpecificationContentIndex.getAllIndexedFiles(element.project)
+//        log.warn("apiFiles {}", apiFiles.toString())
+//
+//        val basePaths = OpenapiSpecificationContentIndex.getBasePathsForFile(file)
+//        log.warn("basePaths {}", basePaths.toString())
+
+        val searchResult = ReferencesSearch.search(file)
+        searchResult.forEach {
+            log.warn("result {}", it.canonicalText)
+            // it.element  => bar.yaml
+            // parent => $ref
+            // parent => ...
+            // parent => /bar
+//            val rrs = it.resolveReference()
+//            rrs.forEach { itrrs ->
+//                log.warn("rrs {}", itrrs.toString())
+//            }
+
+            val referencing = it.element
+            log.warn("referencing {} {}", referencing.text, referencing.containingFile)
+
+            val resolve = it.resolve()
+            log.warn("resolved {} {}", resolve?.text, resolve?.containingFile)
+        }
+
+//        PsiManager.getInstance(element.project).
+//        FileTypeIndex.getFiles()
+//        FilenameIndex.getFilesByName()
+
+//        val paths = keyValue.keyText == "paths"
+
+        // element.parent.parent.parent.text
+        // key start with / and parent is paths:
+
+//        log.warn("yaml paths {}", paths)
     }
 
     object Methods {
