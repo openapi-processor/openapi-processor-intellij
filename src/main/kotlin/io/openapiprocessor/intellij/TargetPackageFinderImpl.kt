865f325f40dd32123f74524448a8430f4537a2a2
Martin Hauner <martin.hauner@gmx.net>
2024-11-17 18:18:18 +0100

    simpler

--- src/main/kotlin/io/openapiprocessor/intellij/TargetPackageFinderImpl.kt
+++ src/main/kotlin/io/openapiprocessor/intellij/TargetPackageFinderImpl.kt
@@ -5,11 +5,10 @@
 
 package io.openapiprocessor.intellij
 
-import com.intellij.openapi.compiler.CompilerPaths
 import com.intellij.openapi.module.Module
-import com.intellij.openapi.module.ModuleManager
-import com.intellij.openapi.roots.ModuleRootManager
-import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.project.guessModuleDir
+import com.intellij.openapi.project.modules
+import com.intellij.openapi.project.rootManager
 import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiPackage
@@ -22,158 +21,104 @@ import org.slf4j.LoggerFactory
  * since the **targetDir** option of the processor is *hidden* in the project build file (gradle
  * or maven) it takes a few steps to find it.
  *
- * First it needs to find the module with the production source code. The mapping file is usually
- * not in the same module but in the "parent" module.
- * Second it needs to find the target package in a source root in the compiler output path of the
- * production source code module. For gradle typically in the *build* folder and for maven in the
+ * 1. lookup the package directories
+ * 2. find the package directories in the module of the mapping.yaml
+ * 3. find the child modules in the module of the mapping.yaml
+ * 4. find the best matching package directory from the source roots (i.e. the longest common prefix)
+ *
+ * The resulting directory should be the generated folder in the gradle *build* folder or in the maven
  * *target* folder.
  */
 class TargetPackageFinderImpl : TargetPackageFinder {
     private val log: Logger = LoggerFactory.getLogger(this.javaClass.name)
 
     override fun findPackageDir(pkgName: String, mappingModule: Module): PsiDirectory? {
-        log.debug("looking for generated pkg '{}' of mapping.yaml in module '{}'", pkgName, mappingModule.name)
+        log.debug("looking for generated pkg '{}' of mapping.yaml in module '{}' of project '{}'",
+            pkgName,
+            mappingModule.name,
+            mappingModule.project.name)
 
-        val pkg = getPackage(mappingModule, pkgName)
-        val modules = findCandidateModules(mappingModule)
+        val modulePath = getModulePath(mappingModule) ?: return null
+        val targetPkg = getTargetPackage(mappingModule, pkgName)
+        val pkgDirs = filterPackageDirs(targetPkg, modulePath)
+        val childModules = filterChildModules(mappingModule, modulePath)
 
-        modules.forEach {
-            val out = getOutputPath(it)
-                ?: return null
-
-            val srcRoots = getSourceRoots(it)
-            val prefixes = findSourceRootsInOutputPath(srcRoots, out)
-            if (prefixes.isEmpty())
-                return null
-
-            val prefix = prefixes.maxByOrNull { p -> p.length }!!
-            val target = findPkgWithPrefix(pkg, prefix)
-            return target
-        }
-
-        return null
-    }
-
-    private fun findPkgWithPrefix(pkg: PsiPackage?, prefix: String): PsiDirectory? {
-        val target: PsiDirectory? = pkg?.directories?.firstOrNull {
-            it.virtualFile.path.startsWith(prefix)
-        }
-
-        if (target == null) {
-            log.debug("did not find target pkg location!")
-            return null
-        }
-
-        log.debug("found target pkg location: {}", target.virtualFile.path)
-        return target
-    }
-
-    private fun findSourceRootsInOutputPath(sourceRoots: Array<VirtualFile>, out: String): List<String> {
-        val prefixes = mutableListOf<String>()
-        sourceRoots.forEach { r ->
-            val prefix = out.commonPrefixWith(r.path)
-            if (prefix.isNotEmpty()) {
-                prefixes.add(prefix)
-            }
-        }
-
-        log.debug("source roots in module output path ({}):", prefixes.size)
-        prefixes.forEach {
-            log.debug(">> {}", it)
-        }
-        return prefixes
-    }
-
-    private fun getSourceRoots(module: Module): Array<VirtualFile> {
-        val sourceRoots = ModuleRootManager.getInstance(module).sourceRoots
-
-        log.debug("module source roots ({}):", sourceRoots.size)
-        sourceRoots.forEach {
-            log.debug(">> {}", it.path)
-        }
-        return sourceRoots
-    }
-
-    private fun getOutputPath(module: Module): String? {
-        val out = CompilerPaths.getModuleOutputPath(module, false)
-        if (out == null) {
-            log.warn("module output path: <unavailable>")
-        }
-        return out
-    }
-
-    private fun findCandidateModules(module: Module): List<Module> {
-        log.debug("looking for nested modules in module '{}' with output path", module.name)
-
-        // target module is usually below this folder
-        val parentRoot = findContentRoot(module) ?: return emptyList()
-
-        return ModuleManager.getInstance(module.project).modules
-            .filter { it.name != module.name }
-            .filter { isNestedModule(it, parentRoot) }
-            .filter { hasOutputPath(it) }
-    }
-
-    private fun hasOutputPath(module: Module): Boolean {
-        val outputPath = CompilerPaths.getModuleOutputPath(module, false)
-
-        if (outputPath == null) {
-            log.debug(">> no output path in module '{}'", module.name)
+        val targetDir = findTargetPackage(childModules, pkgDirs)
+        if (targetDir != null) {
+            log.debug("found matching targetDir pkg {}", targetDir)
         } else {
-            log.debug(">> found output path in module '{}': '{}'", module.name, outputPath)
+            log.debug("found no matching targetDir pkg")
         }
 
-        return outputPath != null
+        return targetDir
     }
 
-    private fun isNestedModule(module: Module, parentRoot: String): Boolean {
-        val nested: Boolean
-
-        val candidateRoots = ModuleRootManager.getInstance(module).sourceRoots
-        nested = candidateRoots.any {
-            it.path.startsWith(parentRoot)
-        }
-
-        if (nested) {
-            log.debug(">> nested module '{}'", module.name)
-        } else {
-            log.debug(">> not nested module '{}'", module.name)
-        }
-
-        return nested
-    }
-
-    private fun findContentRoot(module: Module): String? {
-        val roots = ModuleRootManager.getInstance(module).contentRoots
-            .map {
-                it.path
-            }
-
-        if (roots.isEmpty()) {
-            log.warn("found no content root in module '{}'", module.name)
-            return null
-        }
-
-        if (roots.size > 1) {
-            log.warn("found multiple content roots in module '{}'", module.name)
-            roots.forEach {
-                log.warn(">> {}", it)
-            }
-            return null
-        }
-
-        log.debug("found content root in module '{}': '{}'", module.name, roots.first())
-        return roots.first()
-    }
-
-    private fun getPackage(module: Module, pkgName: String): PsiPackage? {
+    private fun getTargetPackage(module: Module, pkgName: String): PsiPackage? {
         val pkg = JavaPsiFacade.getInstance(module.project).findPackage(pkgName)
 
-        log.debug("found package location candidates ({}):", pkg?.directories?.size)
+        log.debug("found package locations ({}):", pkg?.directories?.size)
         pkg?.directories?.forEach {
             log.debug(">> {}", it.virtualFile.path)
         }
         return pkg
     }
 
+    private fun getModulePath(module: Module): String? {
+        val path = module.guessModuleDir()?.path
+        log.debug("found module path: '{}'", path)
+        return path
+    }
+
+    private fun filterPackageDirs(pkg: PsiPackage?, modulePath: String): List<PsiDirectory> {
+        val dirs = pkg
+            ?.directories
+            ?.filter { it.virtualFile.path.startsWith(modulePath) }
+            .orEmpty()
+
+        log.debug("found packages in module: '{}'", modulePath)
+        dirs.forEach {
+            log.debug(">> '{}'", it.virtualFile.path)
+        }
+
+        return dirs
+    }
+
+    private fun filterChildModules(module: Module, modulePath: String): List<Module> {
+        val matches = module.project.modules
+            .filter { it.guessModuleDir()?.path?.startsWith(modulePath) ?: false }
+
+        log.debug("found nested modules of module: '{}'", modulePath)
+        matches.forEach {
+            log.debug(">> '{}' '{}'", it.name, it.guessModuleDir())
+        }
+
+        return matches
+    }
+
+    private fun findTargetPackage(modules: List<Module>, pkgDirs: List<PsiDirectory>): PsiDirectory? {
+        var commonPrefix = ""
+        var matchingPkg: PsiDirectory? = null
+
+        modules.forEach { module ->
+            log.debug("module '{}' source roots", module.name)
+
+            module.rootManager.sourceRoots.forEach { root ->
+                log.debug(">> source root: {}", root.path)
+
+                pkgDirs.forEach {
+                    val dir = it.virtualFile.path
+
+                    if (dir.startsWith(root.path)) {
+                        val common = dir.commonPrefixWith(root.path)
+                        if (common.length > commonPrefix.length) {
+                            commonPrefix = common
+                            matchingPkg = it
+                        }
+                    }
+                }
+            }
+        }
+
+        return matchingPkg
+    }
 }
