8de915af33e701f0cb2802236bcad0f7f322ba5f
Jakub Chrzanowski <jakub@chrzanowski.info>
2026-04-10 22:57:07 +0200

    Remove redundant `java` plugin declaration from `build.gradle.kts`

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 
 plugins {
-    id("java") // Java support
     alias(libs.plugins.kotlin) // Kotlin support
     alias(libs.plugins.intelliJPlatform) // IntelliJ Platform Gradle Plugin
     alias(libs.plugins.changelog) // Gradle Changelog Plugin
