2d81f88b9b45479e3350ae6b98d3ee3b074d5990
Jakub Chrzanowski <jakub@chrzanowski.info>
2026-04-10 23:05:01 +0200

    Remove redundant `pluginVersion` configuration from `build.gradle.kts`.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -20,6 +20,7 @@
 - Remove redundant `intellijPlatform.publishing.token` configuration from `build.gradle.kts`
 - Remove redundant `intellijPlatform.pluginVerification` configuration from `build.gradle.kts`
 - Remove redundant `java` plugin declaration from `build.gradle.kts`
+- Remove redundant `pluginVersion` configuration from `build.gradle.kts`.
 
 ## [2.4.1] - 2026-03-20
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,6 @@ dependencies {
 intellijPlatform {
     pluginConfiguration {
         name = providers.gradleProperty("pluginName")
-        version = providers.gradleProperty("pluginVersion")
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
         description = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
