66f506d4520f154c3b48870821a05c6327acb1a1
Martin Hauner <martin.hauner@gmx.net>
2026-07-12 15:16:03 +0200

    clean up gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,13 +11,16 @@ dependencies {
     testImplementation(libs.junit.jupiter)
     testImplementation(libs.junit4)
     testRuntimeOnly(libs.junit.launcher)
-//    testImplementation("junit:junit:4.13.2")
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more:
     // https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
-//        intellijIdea("2025.2.6.2")
         intellijIdea("2026.1")
+
+        //intellijIdea("2025.2.6.2")
+        //intellijIdea("253-EAP-SNAPSHOT") {
+        //    useInstaller = false
+        //}
 
         // Plugin Dependencies - https://plugins.jetbrains.com/docs/intellij/plugin-dependencies.html
         bundledPlugins("com.intellij.java", "org.jetbrains.plugins.yaml", "com.intellij.modules.json")
