2984b130356c170741ea3d0a38200847529ca587
Martin Hauner <martin.hauner@gmx.net>
2026-09-17 15:16:41 +0200

    don't need Nullable instrumentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
 // Configure IntelliJ Platform Gradle Plugin - read more:
 // https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-extension.html
 intellijPlatform {
+    instrumentCode = false
     buildSearchableOptions = false
 
     publishing {
